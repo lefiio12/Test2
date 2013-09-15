c8e16779d32323b29235667549599d8430b46156
Adamh <lefiio12@gmail.com>
2013-09-15 14:45:48 +0100

    a

--- autoload.php
+++ autoload.php
@@ -6,24 +6,3 @@ spl_autoload_register(function($class){
     include __DIR__.'/Models/'.$class.'.php';
 });
 
-if( isset($_COOKIE['Token']) && isset($_COOKIE['Timeout']) && isset($_SESSION['Username'])){
-    $userTable = new UsersTable();
-    $user=new User($userTable->fetchToken($_COOKIE['Token']));
-    if($_COOKIE['Token'] == $user->getToken()  &&  ((int)$_COOKIE['Timeout']>time() && (int)$_COOKIE['Timeout']<strtotime("+24 hours")) ){        // Checks that the value of the cookie is the same as the token in the database. If it isnt the cookie is removed and the token in the database is removed. The user is no longer logged in.
-        $_SESSION['LoggedIn'] = true;
-        $_SESSION['LoginTime']=$user->getLastLogin();
-        $_SESSION['Username']=$user->getUsername();
-        $_SESSION['UserID']=$user->getID();
-        if($user->getIsAdmin() == 1){
-            $_SESSION['isAdmin'] = true;
-        }else{
-            $_SESSION['isAdmin'] = false;
-        }
-    }else{
-        header("location: logout.php");
-        
-    }
-    
-}else{
-    $_SESSION['LoggedIn']=false;
-}
--- updateLoation.php
+++ updateLoation.php
@@ -1,10 +1,13 @@
 <?php           
+    require_once('autoload.php');
     $deviceTable=new DeviceTable();
     
     
     $byID=$deviceTable->fetchByPrimaryKey($_GET['id']);
     
     var_dump($byID);
+    
+    echo $byID;
     
     if(isset($_GET['long']) && isset($_GET['lat']) && isset($_GET['id'])){       
         $deviceTable->updateLocation($_GET['id'], $_GET['long'], $_GET['id']);
