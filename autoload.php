7370afc440a4f9826707236ec2bbbd0cd09f3fcb
Adamh <lefiio12@gmail.com>
2013-09-15 14:50:44 +0100

    a

--- autoload.php
+++ autoload.php
@@ -1,7 +1,4 @@
 <?php
-date_default_timezone_set("UTC");
-session_start();
-
 spl_autoload_register(function($class){
     include __DIR__.'/Models/'.$class.'.php';
 });
--- updateLoation.php
+++ updateLoation.php
@@ -5,9 +5,6 @@
     
     $byID=$deviceTable->fetchByPrimaryKey($_GET['id']);
     
-    var_dump($byID);
-    
-    echo $byID;
     
     if(isset($_GET['long']) && isset($_GET['lat']) && isset($_GET['id'])){       
         $deviceTable->updateLocation($_GET['id'], $_GET['long'], $_GET['id']);
