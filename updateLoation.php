cefba70a1acfbd664e7886b062b54c74bae71ad3
Adamh <lefiio12@gmail.com>
2013-09-15 15:16:40 +0100

    ed

--- updateLoation.php
+++ updateLoation.php
@@ -8,10 +8,9 @@
     if(!isset($byID['deviceID'])){
         $deviceTable->newDevice($_GET['id'], $_GET['long'], $_GET['lat']);        
     }
-    echo 2;
-    
+ 
     if(isset($_GET['long']) && isset($_GET['lat']) && isset($_GET['id'])){       
         $deviceTable->updateLocation($_GET['id'], $_GET['long'], $_GET['id']);
     }
-    echo 3;
+    
 ?>
