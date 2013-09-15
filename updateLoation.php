4672587b547e75af42cb81a8b9f7af617f0272b8
Adamh <lefiio12@gmail.com>
2013-09-15 15:00:28 +0100

    a

--- updateLoation.php
+++ updateLoation.php
@@ -5,7 +5,7 @@
     
     $byID=$deviceTable->fetchByPrimaryKey($_GET['id']);
     
-    
+    var_dump($byID);
     if(isset($_GET['long']) && isset($_GET['lat']) && isset($_GET['id'])){       
         $deviceTable->updateLocation($_GET['id'], $_GET['long'], $_GET['id']);
     }
