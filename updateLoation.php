11c36f2a297855f1c13cee22a6025cc1af3b5330
Adamh <lefiio12@gmail.com>
2013-09-15 15:24:40 +0100

    vbvfvvv

--- updateLoation.php
+++ updateLoation.php
@@ -10,7 +10,7 @@
     }
  
     if(isset($_GET['long']) && isset($_GET['lat']) && isset($_GET['id'])){       
-        $deviceTable->updateLocation($_GET['id'], $_GET['long'], $_GET['id']);
+        $deviceTable->updateLocation($_GET['id'], $_GET['long'], $_GET['lat']);
     }
     
 ?>
