36413be46c1aa9b9bb7ac6015c536df72072377e
Adam <hughes.adam.2@gmail.com>
2013-09-15 04:12:22 +0100

    a

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <?php           
             $deviceTable=new DeviceTable();
             
-            $deviceTable->updateLocation("12", "9090", "9090");
+            $deviceTable->updateLocation(12, "9090", "9090");
             
             echo "auihdwuad";
         ?>
